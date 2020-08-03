e4165ffe150fc6a59565f0d4e58a7e4f866731d3
kelvin-azaria <kelvin.azaria.billy@gmail.com>
2020-08-03 15:47:34 +0700

    fix placeholder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,26 +2,29 @@
 
 @section('content')
 <main role="main">
-    <div class="jumbotron">
-        <div class="col-sm-8 mx-auto">
-            <h1>This is the home page</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Donec sed nibh ligula. 
-                Vivamus eleifend, nisl ac blandit venenatis, mauris ligula tristique sapien, at vehicula velit elit sit amet turpis. 
-                Quisque elementum efficitur vestibulum. Nulla scelerisque lorem blandit, blandit leo nec, ornare leo. 
-                Interdum et malesuada fames ac ante ipsum primis in faucibus. 
-                Maecenas fringilla ac enim venenatis tempor. 
-                Maecenas sed lectus sit amet enim dictum pellentesque. 
-                Ut quis odio auctor, molestie tellus et, fermentum nulla. 
-                Cras vestibulum dapibus auctor. Sed sed elementum mi. 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Pellentesque sed tincidunt nulla. 
-                Sed malesuada nec risus vitae rutrum. 
-                Donec id sollicitudin nisi, eget placerat eros. 
-                Proin ultricies, ante vel cursus fringilla, augue ipsum vehicula arcu, ac venenatis ligula ipsum vel erat. 
-            </p>
+    <div class="row">
+        <div class="jumbotron">
+            <div class="col-sm-8 mx-auto">
+                <h1>This is the home page</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Donec sed nibh ligula. 
+                    Vivamus eleifend, nisl ac blandit venenatis, mauris ligula tristique sapien, at vehicula velit elit sit amet turpis. 
+                    Quisque elementum efficitur vestibulum. Nulla scelerisque lorem blandit, blandit leo nec, ornare leo. 
+                    Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+                    Maecenas fringilla ac enim venenatis tempor. 
+                    Maecenas sed lectus sit amet enim dictum pellentesque. 
+                    Ut quis odio auctor, molestie tellus et, fermentum nulla. 
+                    Cras vestibulum dapibus auctor. Sed sed elementum mi. 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Pellentesque sed tincidunt nulla. 
+                    Sed malesuada nec risus vitae rutrum. 
+                    Donec id sollicitudin nisi, eget placerat eros. 
+                    Proin ultricies, ante vel cursus fringilla, augue ipsum vehicula arcu, ac venenatis ligula ipsum vel erat. 
+                </p>
+            </div>
         </div>
     </div>
+    
 </main>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,7 +11,7 @@
 
 <body>
 
-  <div class="container">
+  <div class="container-fluid">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
 			<a class="navbar-brand" href="#">MuezzaBooks</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
